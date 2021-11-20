de2c12b304caffb21c320ade3732a898ae499eb7
Dmitry Shtanko <lempionnewliving@gmail.com>
2021-11-20 20:25:37 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yalagtyarzh/leafsite
+
+go 1.17
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/yalagtyarzh/leafsite/pkg/config"
+	"github.com/yalagtyarzh/leafsite/pkg/models"
+	"github.com/yalagtyarzh/leafsite/render"
+)
+
+//Repo the repository used by the handlers
+var Repo *Repository
+
+//Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+//NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+//NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+//Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+//About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	//perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
