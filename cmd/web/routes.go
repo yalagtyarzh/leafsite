dde68403f341806919953857ef30bf8ab2e1aa1e
Dmitry Shtanko <lempionnewliving@gmail.com>
2021-12-17 13:09:42 +0300

    fix(routes): Added fileServer with static styles

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,5 +23,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Get("/contact", handlers.Repo.Contact)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
