bde63bda3d6ed68c22b180bc5b389cca96a78e2e
Dmitry Shtanko <lempionnewliving@gmail.com>
2021-12-18 18:34:09 +0300

    feat(reservation): Added form validation

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/yalagtyarzh/leafsite/internal/config"
 	"github.com/yalagtyarzh/leafsite/internal/handlers"
+	"github.com/yalagtyarzh/leafsite/internal/models"
 	"github.com/yalagtyarzh/leafsite/internal/render"
 )
 
@@ -19,7 +21,10 @@ var session *scs.SessionManager
 
 //main is the main application function
 func main() {
-	//change this to true when in production
+	//What am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	//Change this to true when in production
 	app.InProduction = false
 
 	session = scs.New()
--- go.mod
+++ go.mod
@@ -8,3 +8,5 @@ require (
 )
 
 require github.com/alexedwards/scs/v2 v2.5.0
+
+require github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
