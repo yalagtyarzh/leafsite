2d9adfdf013c2fd16869cfeaf2fc9e99f52650e1
Dmitry Shtanko <lempionnewliving@gmail.com>
2021-12-24 21:09:02 +0300

    feat(db): Added db handler for PostAvailability

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -32,7 +32,7 @@ func main() {
 	}
 	defer db.SQL.Close()
 
-	fmt.Printf("Starting application on port %s", portNumber)
+	fmt.Printf("Starting application on port %s\n", portNumber)
 
 	srv := &http.Server{
 		Addr:    portNumber,
