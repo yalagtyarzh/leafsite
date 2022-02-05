734e3910b725789d90cc406abbd15ffe328a8936
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-02-05 21:02:54 +0300

    feat(admin): Added reservation process handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/yalagtyarzh/leafsite/internal/config"
 	"github.com/yalagtyarzh/leafsite/internal/driver"
 	"github.com/yalagtyarzh/leafsite/internal/forms"
@@ -632,4 +633,13 @@ func (m *Repository) AdminPostShowReservation(w http.ResponseWriter, r *http.Req
 //AdminReservationsCalendar displays the reservation calendar
 func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-reservations-calendar.page.html", &models.TemplateData{})
+}
+
+//AdminProcessReservation marks a reservation as processed
+func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+	src := chi.URLParam(r, "src")
+	_ = m.DB.UpdateProcessedForReservation(id, 1)
+	m.App.Session.Put(r.Context(), "flash", "Reservation marked as processed")
+	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
 }
